a99c8f9a13b00450375cf86f567703d72d9e9202
Dario <SilverKniff@users.noreply.github.com>
2022-04-01 16:41:52 +0200

    feat/#11 create lobby controller (#12)
    * feat(#9): connect models and entity framework
    
    * feat(#11): add lobby controller
    
    Co-authored-by: Px7-941 <tedbits@mailbox.org>

--- StadtLandFussApi/Models/Lobby.cs
+++ StadtLandFussApi/Models/Lobby.cs
@@ -12,12 +12,12 @@ namespace StadtLandFussApi.Models
 
         [JsonIgnore]
         public int Id { get; set; }
-        [JsonPropertyName("Id")]
-        public string Code { get; set; }
+        [JsonPropertyName("id")]
+        public string? Code { get; set; }
         public List<Category> Categories { get; set; }
         public int Rounds { get; set; }
         public int Timelimit { get; set; }
-        public List<User> Users { get; set; }
+        public List<User>? Users { get; set; }
         public Status Status { get; set; }
 
         #endregion
