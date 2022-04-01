1ed2c06b2c6a68d67745013c6b6ba20a686ded74
Dario <SilverKniff@users.noreply.github.com>
2022-04-01 14:12:43 +0200

    feat/#6 add models
    * feat(#6): add base models
    
    * fix(#6): remove WeatherForecast model references

---
+++ StadtLandFussApi/Models/Answer.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StadtLandFussApi.Models
+{
+    /// <summary>
+    /// An answer specific to a category and a user.
+    /// </summary>
+    public class Answer
+    {
+
+        #region Properties
+
+        [JsonIgnore]
+        public int Id { get; set; }
+        public string Key { get; set; }
+        [JsonPropertyName("Answer")]
+        public string Value { get; set; }
+        public int Downvotes { get; set; }
+        [JsonIgnore]
+        public int UserId { get; set; }
+        [JsonIgnore]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        #endregion
+
+    }
+}
