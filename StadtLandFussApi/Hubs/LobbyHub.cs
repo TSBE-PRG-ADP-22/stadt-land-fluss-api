cc7596e5f159e742827e4922bddf60c326fb21b2
Px7-941 <tedbits@mailbox.org>
2022-05-06 14:45:06 +0200

    Feat/#8 signalr setup (#14)
    * feat(#8): add signalr package and example hub
    
    * add(#8): add test SignalR client
    
    * add(#8): joinlobby
    
    Co-authored-by: Dario <silverkniff@hotmail.com>

---
+++ StadtLandFussApi/Hubs/LobbyHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace StadtLandFussApi.Hubs
+{
+    public class LobbyHub : Hub
+    {
+        public async IAsyncEnumerable<DateTime> Streaming(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                yield return DateTime.UtcNow;
+                await Task.Delay(1000, cancellationToken);
+            }
+        }
+
+        public async Task JoinLobby(string lobbyName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, lobbyName);
+            await Clients.Group(lobbyName).SendAsync("JoinLobby", Context.User.Identity.Name + " joined.");
+        }
+
+        public Task LeaveLobby(string lobbyName)
+        {
+            return Groups.RemoveFromGroupAsync(Context.ConnectionId, lobbyName);
+        }
+    }
+}
