2f8f46c725ea3bfbf370cfd00b881b7ea209775c
TedBits <tedbits@mailbox.org>
2022-05-13 12:11:13 +0200

    fix(#15): move test client to sub directory

---
+++ TestClient/TestClient/Models/Lobby.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace TestClient.Models
+{
+    /// <summary>
+    /// Represents the entire game lobby with users and answers.
+    /// </summary>
+    public class Lobby
+    {
+        public string? Id { get; set; }
+        public List<Category>? Categories { get; set; }
+        public int Rounds { get; set; }
+        public int Timelimit { get; set; }
+        public List<User>? Users { get; set; }
+        public Status Status { get; set; }
+    }
+}
--- TestClient/TestClient/ViewModels/MainWindowViewModel.cs
+++ TestClient/TestClient/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Reactive;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
@@ -13,16 +12,15 @@ namespace TestClient.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public ReactiveCommand<Unit, Unit> ConnectCommand { get; }
-        public ReactiveCommand<Unit, Unit> DisconnectCommand { get; }
-        public ReactiveCommand<Unit, Unit> JoinLobbyCommand { get; }
-
-
         private HubConnection? HubConnection { get; set; }
 
         [Reactive] public string LobbyId { get; set; } = "e97be7b7";
         [Reactive] public string UserId { get; set; } = "24ef415c-2ae8-464d-b958-13f105cf4aef";
         [Reactive] public string Greeting { get; private set; } = "Welcome to Avalonia!";
+
+        public ReactiveCommand<Unit, Unit> ConnectCommand { get; }
+        public ReactiveCommand<Unit, Unit> DisconnectCommand { get; }
+        public ReactiveCommand<Unit, Unit> JoinLobbyCommand { get; }
 
         public MainWindowViewModel()
         {
